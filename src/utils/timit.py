f496c5acd165ff1cfea9cdbcdcd6ef008a482f8e
Jon Gauthier <jon@gauthiers.net>
2024-03-06 16:38:54 -0800

    overhaul of timit representation. fix syllabic consonant representation; handling of glottal stops and closures; sanity-check mapping between flat and grouped phonemic representation. will need to restart pipeline with this new dataset

--- src/utils/timit.py
+++ src/utils/timit.py
@@ -1,4 +1,5 @@
 import itertools
+import logging
 from pathlib import Path
 import re
 
@@ -14,21 +15,29 @@ from src.utils import syllabifier
 
 import numpy as np
 
+L = logging.getLogger(__name__)
+
 
 # TODO cross-check resulting mappings with cmudict
 TIMIT_MAPPING = {
     'ax': 'AH',
     'ax-h': 'AH',
     'axr': 'ER',
+
+    # tap/flap
     'dx': 'T',
-    'el': ['AH', 'L'],
-    'em': ['AH', 'M'],
-    'en': ['AH', 'N'],
-    'eng': ['IH', 'NG'],
+
+    # lateral/nasal release
+    # TODO we should retain this detail for the syllabification somehow
+    'el': "L",
+    'em': "M",
+    'en': "N",
+    'eng': "NG",
     'hv': 'HH',
     'ix': 'IH',
-    'nx': ['N', 'T'],
-    'q': 'T',
+
+    # nasal flap
+    'nx': "N",
     'pau': '[SIL]',
     'epi': '[SIL]',
     'ux': 'UW'
@@ -36,7 +45,16 @@ TIMIT_MAPPING = {
 
 
 TIMIT_IGNORE = ["h#"]
-TIMIT_JOIN_CLOSURES = ['bcl', 'dcl', 'gcl', 'kcl', 'pcl', 'tcl']
+
+# If we see `key` followed by a phone matching regex `value`, join these into a single
+# phone with label equivalent to the label of the second phone
+TIMIT_JOINS = {
+    # join glottal stop with following vowel
+    "q": "^(iy|ih|eh|ey|ae|aa|aw|ay|ah|ao|oy|ow|uh|uw|ux|er|ax|ix|axr|ax-h)$",
+
+    # join plosive closure with release
+    **{f"{k}cl": f"^{k}$" for k in ['b', 'd', 'g', 'k', 'p', 't']}
+}
 
 
 def map_timit_phone_to_cmudict_phoneme(phone, phone_vocab):
@@ -55,37 +73,84 @@ def map_timit_phone_to_cmudict_phoneme(phone, phone_vocab):
 def map_timit_to_cmudict(timit_item, phone_vocab):
     phonemes = []
 
-    i = 0
     phonetic_detail = timit_item["phonetic_detail"]
     num_phones = len(phonetic_detail["start"])
-    while i < num_phones:
+
+    def handle_join_exception(join_key, position) -> tuple[int, list[tuple[int, int, str]]]:
+        """
+        This function is called when we fail to find a partner for a join key -- for example,
+        a closure `dcl` without an accompanying release `d`.
+        
+        Returns:
+            increment: the delta that should be applied to TIMIT phone cursor
+            phonemes: a list of tuples (start, stop, phoneme) that should be appended to the phonemes list
+        """
+        start = phonetic_detail["start"][position]
+        stop = phonetic_detail["stop"][position]
+        if join_key.endswith("cl"):
+            # unreleased stop -- map to full phoneme
+            phonemes = [(start, stop, phoneme)
+                        for phoneme in map_timit_phone_to_cmudict_phoneme(join_key[:-len("cl")], phone_vocab)]
+            return -1, phonemes
+        elif join_key == "q":
+            # treat this as a glottalized /t/
+            # this is often wrong in the annotation, i.e. we'll see a glottal stop preceding a vowel
+            # actually outside of the span of the word containing that vowel.
+            # this code will mark that glottal stop ars a /t/, but that marking
+            # will be disposed later in the pipeline since it's outside of the span
+            # of any word in the sentence
+            L.info(f"Interpreting glottal stop as /t/ in item {timit_item['text']}: "
+                   f"{' '.join(timit_item['phonetic_detail']['utterance'][max(0, position - 2):position+3])}")
+            
+            phonemes = [(start, stop, phoneme)
+                        for phoneme in map_timit_phone_to_cmudict_phoneme("t", phone_vocab)]
+            return -1, phonemes
+        else:
+            L.error(timit_item['text'])
+            L.error(timit_item['phonetic_detail']['utterance'][max(0, position - 2):position+3])
+            L.error(timit_item['phonetic_detail']['utterance'])
+            raise ValueError("error processing join candidate")
+
+    # Process backwards to handle join candidates easily
+    i = num_phones - 1
+    while i >= 0:
         phone = phonetic_detail["utterance"][i]
         start = phonetic_detail["start"][i]
         stop = phonetic_detail["stop"][i]
 
-        if phone in TIMIT_IGNORE:
-            i += 1
-        elif phone in TIMIT_JOIN_CLOSURES:
-            release_phone = phone[:-len("cl")]
-            if phonetic_detail["utterance"][i + 1] == release_phone:
-                phoneme_start = start
-                phoneme_end = phonetic_detail["stop"][i + 1]
-                phoneme_label = map_timit_phone_to_cmudict_phoneme(release_phone, phone_vocab)
+        prev_phone = phonetic_detail["utterance"][i - 1] if i > 0 else None
 
+        if phone in TIMIT_IGNORE:
+            i -= 1
+        elif phone in TIMIT_JOINS:
+            # first element of a join found but we hadn't seen the second element previously
+            increment, join_phonemes = handle_join_exception(phone, i)
+            phonemes.extend(join_phonemes)
+            i += increment
+        elif prev_phone in TIMIT_JOINS:
+            # is the previous phone annotated as being in the same word as the current phone?
+            prev_phone_start = phonetic_detail["start"][i - 1]
+            cur_word_start = max(word_start for word_start in timit_item["word_detail"]["start"]
+                                 if word_start <= start)
+
+            if prev_phone_start >= cur_word_start and re.match(TIMIT_JOINS[prev_phone], phone):
+                phoneme_start = phonetic_detail["start"][i - 1]
+                phoneme_end = stop
+                phoneme_label = map_timit_phone_to_cmudict_phoneme(phone, phone_vocab)
                 for phoneme in phoneme_label:
                     phonemes.append((phoneme_start, phoneme_end, phoneme))
-                i += 2
+
+                i -= 2
             else:
-                phoneme_label = map_timit_phone_to_cmudict_phoneme(release_phone, phone_vocab)
-                for phoneme in phoneme_label:
+                for phoneme in map_timit_phone_to_cmudict_phoneme(phone, phone_vocab):
                     phonemes.append((start, stop, phoneme))
-                i += 1
+                i -= 1
         else:
             for phoneme in map_timit_phone_to_cmudict_phoneme(phone, phone_vocab):
                 phonemes.append((start, stop, phoneme))
-            i += 1
+            i -= 1
 
-    return phonemes
+    return phonemes[::-1]
 
 
 def add_phonemic_detail(item, phone_vocab):
@@ -101,21 +166,39 @@ def add_phonemic_detail(item, phone_vocab):
     return item
 
 
-def group_phonetic_detail(item, drop_phones=None, key="phonetic_detail"):
+def group_phonetic_detail(item, idx, drop_phones=None, key="phonetic_detail"):
     """
     Group phonetic_detail entries according to the containing word.
     """
     phonetic_detail = item[key]
     word_detail = item["word_detail"]
 
+    # Assure that each phone gets mapped to exactly one word. We'll arbitrarily map to the
+    # first word that contains the phone; this seems to most frequently match TIMIT annotation standards
+    phone_mask = np.zeros(len(phonetic_detail["start"]), dtype=bool)
+    # Note that we also assign phonemes which span words to the leftmost word, consistent
+    # with this strategy
+
     word_phonetic_detail = []
-    for start, stop in zip(word_detail["start"], word_detail["stop"]):
+    for start, stop, word in zip(word_detail["start"], word_detail["stop"], word_detail["utterance"]):
         word_phonetic_detail.append([])
-        for phon_start, phon_stop, phon in zip(phonetic_detail["start"], phonetic_detail["stop"], phonetic_detail["utterance"]):
-            if drop_phones is not None and phon in drop_phones:
+        for j, (phon_start, phon_stop, phon) in enumerate(zip(phonetic_detail["start"], phonetic_detail["stop"], phonetic_detail["utterance"])):
+            if phone_mask[j]:
                 continue
-            if phon_start >= start and phon_stop <= stop:
+            elif drop_phones is not None and phon in drop_phones:
+                phone_mask[j] = True
+                continue
+            
+            # if the phoneme has start in this word, assign it to this word
+            if phon_start >= start and phon_start < stop:
+                phone_mask[j] = True
                 word_phonetic_detail[-1].append({"phone": phon, "start": phon_start, "stop": phon_stop})
+
+    for unused_phone in np.flatnonzero(~phone_mask):
+        preceding_phones = " ".join(phonetic_detail["utterance"][max(0, unused_phone - 3):unused_phone])
+        following_phones = " ".join(phonetic_detail["utterance"][unused_phone + 1:min(len(phonetic_detail["utterance"]), unused_phone + 4)])
+        unused_phone_str = phonetic_detail["utterance"][unused_phone]
+        L.warning(f"Unused phone {unused_phone_str} in item {idx} ({item['text']}) (preceding: {preceding_phones}, following: {following_phones})")
 
     item[f"word_{key}"] = word_phonetic_detail
     return item
@@ -169,7 +252,7 @@ def add_syllabic_detail(item):
     return item
 
 
-def check_item(item):
+def check_item(item, idx, drop_phones=None):
     grouped_phonemic_detail = item["word_phonemic_detail"]
     grouped_syllable_detail = item["word_syllable_detail"]
     assert len(grouped_phonemic_detail) == len(item["word_detail"]["utterance"])
@@ -178,6 +261,14 @@ def check_item(item):
     all_phonemes = [phon["phone"] for word in grouped_phonemic_detail for phon in word]
     all_phonemes_syll = [phone for word in item["word_syllable_detail"] for syllable in word for phone in syllable["phones"]]
     assert len(all_phonemes) == len(all_phonemes_syll)
+    assert all_phonemes == all_phonemes_syll, "phonemic detail does not match phonemes within syllable detail"
+
+    # NB we do expect a mismatch here since some phonemes in the flat representation
+    # won't appear in the word grouped representation, if they are outside the span of a word
+    # all_phonemes_flat = [ph for ph in item["phonemic_detail"]["utterance"] if ph not in (drop_phones or [])]
+    # assert all_phonemes == all_phonemes_flat, \
+    #     f"grouped phonemic detail does not match non-grouped phonemic detail in item {idx}:" \
+    #     f"\n{item['text']}\n{all_phonemes}\n{all_phonemes_flat}"
 
 
 def prepare_timit_corpus(data_dir,
@@ -205,9 +296,9 @@ def prepare_timit_corpus(data_dir,
                         fn_kwargs=dict(phone_vocab=phone_vocab))
 
     # Compute phonetic and phonemic details grouped by word span
-    corpus = corpus.map(group_phonetic_detail, batched=False,
+    corpus = corpus.map(group_phonetic_detail, batched=False, with_indices=True,
                         fn_kwargs=dict(drop_phones=drop_phones))
-    corpus = corpus.map(group_phonetic_detail, batched=False,
+    corpus = corpus.map(group_phonetic_detail, batched=False, with_indices=True,
                         fn_kwargs=dict(key="phonemic_detail",
                                        drop_phones=drop_phones))
     
@@ -215,21 +306,14 @@ def prepare_timit_corpus(data_dir,
     corpus = corpus.map(add_syllabic_detail, batched=False)
 
     # Run sanity checks on updated annotations
-    corpus.map(check_item, batched=False)
+    corpus.map(check_item, batched=False, with_indices=True,
+               fn_kwargs=dict(drop_phones=drop_phones))
     
     def prepare_audio(batch):
         audio = batch["audio"]
         batch["input_values"] = processor(audio["array"], sampling_rate=audio["sampling_rate"]).input_values[0]
         return batch
     corpus = corpus.map(prepare_audio)
-
-    if add_phoneme_targets:
-        # TODO stale code
-        twfe = TilingWordFeatureExtractor2(processor.tokenizer, item_key="word_phonemic_detail")
-        def add_features(example):
-            example["phone_targets"] = twfe(example)
-            return example
-        corpus = corpus.map(add_features, load_from_cache_file=False)
     
     return corpus
 
